af6d50bd69ab8721393c8427fb741f8c7e0a05a9
Anuj Patel <anuj@5edev.com>
2023-01-20 17:01:33 +0530

    20-1-22

--- triangle_type.rb
+++ triangle_type.rb
@@ -1,8 +1,12 @@
 class TriangleType 
   def get_triangle_type(arr)
-     result= ( arr.length != 3 ? "not a triangle" : arr.uniq.length == 3 ? "Scalene" : arr.uniq.length == 2 ? "Isosceles" : arr.uniq.length == 1 ? "Equilateral" : "")
+     result= ( arr.length != 3 ? 
+      "not a triangle" : arr.uniq.length == 3 ? 
+      "Scalene" : arr.uniq.length == 2 ? 
+      "Isosceles" : arr.uniq.length == 1 ? 
+      "Equilateral" : "")
   end
 end
 
-arr = [5, 3, 2]
+arr = [4, 4]
 print TriangleType.new.get_triangle_type(arr)
